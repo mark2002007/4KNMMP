58e6f034833ccf0dbd9563b1f070aa5f4a1a98e7
Markiian Mandzak <markmandzak2002@gmail.com>
2023-03-09 14:36:49 +0200

    2.0.0

--- group_project_semester_1.py
+++ group_project_semester_1.py
@@ -6,30 +6,34 @@ VERBOSE = False
 
 def main():
     #================================================== 
-    dots_per_axis = 16
+    dots_per_axis = 17
     dots = get_dots(dots_per_axis, X_RANGE, Y_RANGE)
     dots_num = dots.shape[0] * dots.shape[1]
     dots_map              = get_dots_map(dots)
     dots_map_reverse      = {tuple(dot): dot_num for dot_num, dot in dots_map.items()}#
     triangles             = get_triangles(dots)
+    triangles_num         = len(triangles)
     triangles_map         = get_triangles_map(triangles)
     triangles_map_reverse = {tuple(map(tuple, triangle)) : triangle_num for triangle_num, triangle in triangles_map.items()}
     #================================================== 
-    x1, x2 = sp.symbols("x1:3") 
+    x1, x2 = x = sp.symbols("x1:3") 
     a_11, a_22, d = 1, 1, 2
     u = (x1-2)**2*(x2-2)**2*x1*x2
-    f = -a_11*grad(u)[0] -a_22*grad(u)[1] + u
+    #u = (x1 - 1)*(x2 - 1)*x1*x2**2
+    f = -a_11*grad(u)[0] -a_22*grad(u)[1] + d*u
     print(f"[a_11, a_22, d] : {[a_11, a_22, d]}")
     print(f"f : {f}")
-    A = np.zeros((dots_num,)*2)
+    u = sp.lambdify(x, u)
+    f = sp.lambdify(x, f)
+    A = np.zeros((dots_num, dots_num))
     B = np.zeros((dots_num,1))
+    delta = 2*S(triangles[0])
     for counter, triangle in enumerate(triangles):
-        print(f"{counter+1}/{len(triangles)}")
+        print(f"{counter+1}/{triangles_num}")
         dot_nums = [dots_map_reverse[tuple(dot)] for dot in triangle] #Numbers of dots of current triangle
         #Find ABCe tensors
-        delta = 2*S(triangle)
-        Me = M(d, delta)
         Ke = K(triangle, delta, a_11, a_22)
+        Me = M(d, delta)
         Ae = Ke + Me
         fe = subs(f, triangle)[:, None]
         Qe = Q(Me, d, fe)  
--- lib.py
+++ lib.py
@@ -5,6 +5,15 @@ np.set_printoptions(linewidth=np.inf)
 #np.set_printoptions(threshold=sys.maxsize)
 import sympy as sp
 import matplotlib.pyplot as plt
+import inspect
+
+###Output
+def print_title(title, padding=10): #Prints formated text
+    title_l = len(title)
+    print_width = padding*2 + title_l 
+    print('#'*print_width)
+    print(f"{'#'*padding}{title}{'#'*padding}")
+    print('#'*print_width)
 
 ###Triangulation
 def get_dots(dots_per_axis, x_range, y_range): #Calculates matrix of dots where each dot is [x, y]
@@ -22,40 +31,35 @@ def get_triangles(dots): #Generates vector of triangles where each triangle is v
     for yi in range(len(dots) - 1):
         for xi in range(len(dots[yi]) - 1):
             if (xi, yi) in [(0, 0), (len(dots[xi])-2, len(dots[xi])-2)]:
-                triangles.append([dots[yi, xi], dots[yi+1, xi+1], dots[yi+1, xi  ]])
-                triangles.append([dots[yi, xi], dots[yi, xi+1]  , dots[yi+1, xi+1]])
+                triangles.append([dots[yi, xi]  , dots[yi+1, xi+1], dots[yi+1, xi  ]])
+                triangles.append([dots[yi, xi]  , dots[yi, xi+1]  , dots[yi+1, xi+1]])
             else:
-                triangles.append([dots[yi+1, xi], dots[yi, xi]    , dots[yi, xi+1]])
+                triangles.append([dots[yi, xi]  , dots[yi, xi+1]  , dots[yi+1, xi]])
                 triangles.append([dots[yi, xi+1], dots[yi+1, xi+1], dots[yi+1, xi]])
     return np.array(triangles)
 
 def get_triangles_map(triangles): #Generages mapping from triangle number to triangle
-    return dict(enumerate(triangles))
+    return dict(enumerate(triangles)) 
 
-def inner_outer_dot_nums(dots, dots_map, dots_map_reverse):
-    inner_dot_nums = [dots_map_reverse[tuple(dot)] for dot in dots[1:-1, 1:-1].reshape(-1, 2)]
+def inner_outer_dot_nums(dots, dots_map, dots_map_reverse): #Returns numbers of dots separated on inner and outer
+    inner_dot_nums = [dots_map_reverse[tuple(dot)] for dot in dots[1:-1, 1:-1].reshape(-1, dots.shape[-1])]
     outer_dot_nums = list(set(range(dots.shape[0] * dots.shape[1])) - set(inner_dot_nums))
     return inner_dot_nums, outer_dot_nums
 
-###Output
-def print_title(title, padding=10): #Prints formated text
-    title_l = len(title)
-    print_width = padding*2 + title_l 
-    print('#'*print_width)
-    print(f"{'#'*padding}{title}{'#'*padding}")
-    print('#'*print_width)
+def extract_boundary_dots(triangle, x_range, y_range):
+    return np.array([dot for dot in triangle if dot[0] in x_range or dot[1] in y_range])
 
+def is_boundary_el(boundary_dots): #TODO : test this function and also check triangles countercloswise
+    return 2 == len(boundary_dots)
+
+def which_boundary(boundary_dots, x_range, y_range):
+    bd = boundary_dots
+    if   bd[0,0] == bd[1,0] == x_range[0]: return 3
+    elif bd[0,0] == bd[1,0] == x_range[1]: return 1
+    elif bd[0,1] == bd[1,1] == y_range[0]: return 0
+    elif bd[0,1] == bd[1,1] == y_range[1]: return 2
+    else: raise Exception("ConditionException")
 ###Matrices
-def free_symbols(f): #Extracts variables sorted by name from function (eg. f=2*x2+x1**2 ==> [x1, x2])
-    return sorted(f.free_symbols, key=lambda s: s.name)
-
-def subs(f, sub): #Passes elements along last axis into sympy function (eg. f=x1*x2 and sub=[[[1, 2],[3, 4]]] ==> [[2, 12]])
-    func = lambda xs: f.subs(zip(free_symbols(f), xs))
-    return np.apply_along_axis(func1d=func, axis=-1, arr=sub).astype(np.float64)
-
-def grad(f): #Calculates sympy gradient (eg. f=x1**2+3*x2**2 ==> np.array([2, 6]))
-    return np.array([sp.diff(f, var, var) for var in free_symbols(f)])
-
 def S(triangle): #Calculates area of triangle (eg. triangle=np.array([[0, 0],[1, 0],[0, 1]]) ==> 0.5)
     matrix = np.hstack((np.ones((3, 1)), triangle))
     return .5*np.linalg.det(matrix)
@@ -77,5 +81,23 @@ def M(d, delta): #Calculates M_e matrix (btw. np.eye(3) + 1 returns np.array([[2
     return (d*delta/24)*(np.eye(3)+1)
 
 def Q(Me, d, fe): #Calculates Q_e matrix
-    return Me/d@fe
+    return (Me/d)@fe
 
+def R(sigma, beta, Gamma):
+    return sigma*Gamma/(beta*6)*(np.eye(2) + 1)
+
+def P(psi, beta, Gamma):
+    return psi*Gamma/(beta*2)*np.ones((2, 1))
+###Helpers
+def free_symbols(f): #Extracts variables sorted by name from function (eg. f=2*x2+x1**2 ==> [x1, x2])
+    return sorted(f.free_symbols, key=lambda s: s.name)
+
+def subs(f, sub): #Passes elements along last axis into sympy function (eg. f=x1*x2 and sub=[[[1, 2],[3, 4]]] ==> [[2, 12]])
+    if inspect.isfunction(f):
+        func = lambda xs: f(*xs)
+    else:
+        func = lambda xs: f.subs(zip(free_symbols(f), xs))
+    return np.apply_along_axis(func1d=func, axis=-1, arr=sub).astype(float)
+
+def grad(f): #Calculates sympy gradient (eg. f=x1**2+3*x2**2 ==> np.array([2, 6]))
+    return np.array([sp.diff(f, var, var) for var in free_symbols(f)])
